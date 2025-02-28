3c0cf7f43558522f0a098df0b9f5dff90bc232dc
anthonynmh <anthonyneomh@gmail.com>
2025-02-28 22:08:01 +0800

    Add persistent local storage for habit list

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 import 'package:habit_tracker_app/screens/habit_list_page.dart';
 import 'package:habit_tracker_app/widgets/input_dialog.dart';
 
 class DashboardPage extends StatefulWidget {
-  const DashboardPage({
-    super.key,
-  });
+  const DashboardPage({super.key});
 
   @override
   State<DashboardPage> createState() => _DashboardPageState();
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  final habitsList = <String>[];
+  List<String> habitsList = [];
   bool isDeleteMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHabits();
+  }
+
+  Future<void> _loadHabits() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      habitsList = prefs.getStringList('habits') ?? []; // Defaults to empty list
+    });
+  }
+
+  Future<void> _saveHabits() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    if (habitsList.isEmpty) {
+      await prefs.remove('habits'); // Remove key if list is empty
+    } else {
+      await prefs.setStringList('habits', habitsList);
+    }
+  }
 
   void _addHabit(String habit) {
     setState(() {
       habitsList.add(habit);
     });
+    _saveHabits();
   }
 
   void _deleteHabit(int index) {
     setState(() {
       habitsList.removeAt(index);
     });
+    _saveHabits();
+
+    // default to non-deleteMode when no habits
+    if (habitsList.isEmpty) {
+      _toggleDeleteMode();
+    }
   }
 
   void _toggleDeleteMode() {
@@ -39,13 +69,13 @@ class _DashboardPageState extends State<DashboardPage> {
       appBar: AppBar(
         title: Text("All Habits"),
         actions: habitsList.isNotEmpty
-          ? [
-              IconButton(
-                icon: Icon(isDeleteMode ? Icons.cancel : Icons.delete),
-                onPressed: _toggleDeleteMode,
-              ),
-            ]
-          : null,
+            ? [
+                IconButton(
+                  icon: Icon(isDeleteMode ? Icons.cancel : Icons.delete),
+                  onPressed: _toggleDeleteMode,
+                ),
+              ]
+            : null,
       ),
       body: HabitList(
         habitsList: habitsList,
